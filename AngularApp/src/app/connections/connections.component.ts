2fd1bbcdbe3a64e464d1f9e0ec8bdd5a7a7e78ba
Sean Odnert <Sean.Odnert@intapp.com>
2017-08-24 15:14:23 -0700

    Added client on click functionality

--- AngularApp/src/app/connections/connections.component.ts
+++ AngularApp/src/app/connections/connections.component.ts
@@ -2,10 +2,23 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  templateUrl: 'connections.component.html'
+  templateUrl: 'connections.component.html',
+  styleUrls: ['connections.component.css']
 })
 export class ConnectionsComponent {
 
   constructor( ) { }
+
+  public isClientView: boolean = false;
+
+  public clientClicked(){
+    console.log("client clicked");
+    this.isClientView = true;
+  }
+
+  public backButtonClicked(){
+    console.log("back clicked");
+    this.isClientView = false;
+  }
 
 }
