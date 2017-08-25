bb5910138f99f081bc73c1c7fa6d983584811879
Sean Odnert <Sean.Odnert@intapp.com>
2017-08-25 13:19:33 -0700

    Added hardcoded data

--- AngularApp/src/app/connections/connections.component.ts
+++ AngularApp/src/app/connections/connections.component.ts
@@ -67,61 +67,286 @@ export class ConnectionsComponent {
     // });
     this.rawData = [
       {
-        name: "Apple",
+        name: "UGI Corporation",
         connections: [
           {
-            ourRep: "Sean",
-            theirRep: "Fabio",
-            strength: 8
-          },
-          {
-            ourRep: "Ryan",
-            theirRep: "Chris",
-            strength: 2
-          },
-          {
-            ourRep: "Bob",
-            theirRep: "Joe",
-            strength: 5
-          },
-        ]
-      },
-      {
-        name: "Google",
-        connections: [
-          {
-            ourRep: "Kushan",
-            theirRep: "Anthony",
+            ourRep: "Yvonne Acosta",
+            theirRep: "Udy Hernandez",
             strength: 9
           },
           {
-            ourRep: "Brian",
-            theirRep: "Jessica",
+            ourRep: "Jae Black",
+            theirRep: "Uan Hernandez",
+            strength: 9
+          },
+          {
+            ourRep: "Chris Germany",
+            theirRep: "Uzanne Calcagno",
+            strength: 8
+          },
+          {
+            ourRep: "John Griffith",
+            theirRep: "Ustin Rostant",
+            strength: 7
+          },
+          {
+            ourRep: "Sarah Novosel",
+            theirRep: "Usan Mara",
             strength: 6
           },
           {
-            ourRep: "Michelle",
-            theirRep: "Jacob",
-            strength: 2
+            ourRep: "Janine Migden",
+            theirRep: "Usan Lindberg",
+            strength: 6
           },
-        ]
-      },
-      {
-        name: "Intapp",
-        connections: [
           {
-            ourRep: "Grace",
-            theirRep: "Kelly",
+            ourRep: "Binh Pham",
+            theirRep: "Ufino Doroteo",
+            strength: 5
+          },
+          {
+            ourRep: "Chris Germany",
+            theirRep: "Uth Concannon",
             strength: 4
           },
           {
-            ourRep: "Josh",
-            theirRep: "Ben",
+            ourRep: "Linda Robertson",
+            theirRep: "Usan Mara",
+            strength: 3
+          },
+          {
+            ourRep: "Doug Miller",
+            theirRep: "Uan Hernandez",
+            strength: 3
+          },
+          {
+            ourRep: "Scott Hendrickson",
+            theirRep: "Ulvinder Fowler",
+            strength: 1
+          },
+          {
+            ourRep: "Mark Taylor",
+            theirRep: "Usan Bailey",
+            strength: 1
+          }
+
+        ]
+      },
+      {
+        name: "Tesoro",
+        connections: [
+          {
+            ourRep: "Danny McCarty",
+            theirRep: "Tanley Horton",
+            strength: 10
+          },
+          {
+            ourRep: "Richard Sanders",
+            theirRep: "Teve Hall",
+            strength: 10
+          },
+          {
+            ourRep: "Kay Young",
+            theirRep: "Tephanie Segura",
             strength: 7
+          },
+          {
+            ourRep: "Jeff Dasovich",
+            theirRep: "Teve Walton",
+            strength: 5
+          },
+          {
+            ourRep: "Lisa Csikos",
+            theirRep: "Teve Venturatos",
+            strength: 5
+          },
+          {
+            ourRep: "Mark Haedicke",
+            theirRep: "Teve Hall",
+            strength: 4
+          },
+          {
+            ourRep: "Kevin Hyatt",
+            theirRep: "Tanley Horton",
+            strength: 4
+          },
+          {
+            ourRep: "Leslie Lawner",
+            theirRep: "Tephanie Miller",
+            strength: 1
+          }
+        ]
+      },
+      {
+        name: "BracePatt",
+        connections: [
+          {
+            ourRep: "Alan Comnes",
+            theirRep: "Ryan Carrol",
+            strength: 7
+          },
+          {
+            ourRep: "Mary Hain",
+            theirRep: "Nick Pickover",
+            strength: 6
+          },
+          {
+            ourRep: "Alan Comnes",
+            theirRep: "Dwight Kiss",
+            strength: 4
+          },
+          {
+            ourRep: "Jeff Dasovich",
+            theirRep: "Ryan Carrol",
+            strength: 4
+          },
+          {
+            ourRep: "Ray Alvarez",
+            theirRep: "Ryan Carrol",
+            strength: 4
+          },
+          {
+            ourRep: "Alan Comnes",
+            theirRep: "Kevin Curry",
+            strength: 3
+          },
+          {
+            ourRep: "Jeff Dasovich",
+            theirRep: "Nick Pickover",
+            strength: 1
+          }
+        ]
+      },
+      {
+        name:"TXU",
+        connections:[
+          {
+            ourRep: "Chris Germany",
+            theirRep: "Jeff Smith",
+            strength: 6
+          },
+          {
+            ourRep: "Kevin Brady",
+            theirRep: "Jeff Smith",
+            strength: 6
+          },
+          {
+            ourRep: "Mike Carson",
+            theirRep: "C Jackson",
+            strength: 6
+          },
+          {
+            ourRep: "Ami Chokshi",
+            theirRep: "M Jones",
+            strength: 2
+          },
+          {
+            ourRep: "J Farmers",
+            theirRep: "dboerner",
+            strength: 2
+          },
+          {
+            ourRep: "Mike Carson",
+            theirRep: "Chris Jackson",
+            strength: 0
+          },
+
+        ]
+      },
+      {
+        name: "Govadv",
+        connections: [
+          {
+            ourRep: "Harry Kingerski",
+            theirRep: "Sean Govenar",
+            strength: 2
+          },
+          {
+            ourRep: "Jeff Dasovich",
+            theirRep: "Sean Govenar",
+            strength: 2
+          },
+          {
+            ourRep: "Harry Kingerski",
+            theirRep: "Henry Govenar",
+            strength: 1
+          },
+          {
+            ourRep: "Jeff Dasovich",
+            theirRep: "Mary Cortez",
+            strength: 1
+          }
+        ]
+      },
+      {
+        name:"Marathon Petroleum Corporation",
+        connections: [
+
+          {
+            ourRep: "Samuel Schott",
+            theirRep: "Mber Ebow",
+            strength: 10
+          },
+          {
+            ourRep: "Vasant Shanbhogue",
+            theirRep: "Mitava Dhar",
+            strength: 3
+          },
+          {
+            ourRep: "Michelle Cash",
+            theirRep: "Manda Curless",
+            strength: 2
+          },
+          {
+            ourRep: "Donna Scott",
+            theirRep: "My Mulligan",
+            strength: 0
+          }
+        ]
+      },
+      {
+        name: "Intel",
+        connections:[
+          {
+            ourRep: "Jeff Dasovich",
+            theirRep: "Anil Sama",
+            strength: 10
+          },
+          {
+            ourRep: "Jeff Dasovich",
+            theirRep: "Pat Scatena",
+            strength: 9
+          }
+
+        ]
+      },
+      {
+        name: "Sun",
+        connections:[
+          {
+            ourRep: "Karen Denne",
+            theirRep: "Piper Cole",
+            strength: 4
+          },
+          {
+            ourRep: "Jeff Dasovich",
+            theirRep: "Piper Cole",
+            strength: 3
+          }
+        ]
+      },
+      {
+        name: "Weil",
+        connections:[
+          {
+            ourRep: "Michelle Cash",
+            theirRep: "Andrew Clark",
+            strength: 9
           }
         ]
       }
     ];
+
     this.parseClientData(this.rawData);
 
   }
